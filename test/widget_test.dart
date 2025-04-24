ed2d5c5d760c76bfb4cc021041586203d651088a
mayor itua <ituadavid200527@gmail.com>
2025-04-25 00:19:14 +0100

    maindart correction

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:swift_menu/main.dart';
+import 'package:menu_swift/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
